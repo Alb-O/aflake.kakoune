207b2e367f81741f4625ef633ba79cbbeba8fff1
Albert O'Shea <alberoshea2@gmail.com>
2025-09-13 14:29:21 +0000

    feat: add desktop entry for kakoune

--- nix/packages/kakoune/wrapped.nix
+++ nix/packages/kakoune/wrapped.nix
@@ -1,7 +1,8 @@
-{ inputs
-, lib
-, system
-, ...
+{
+  inputs,
+  lib,
+  system,
+  ...
 }@pkgs:
 let
   fullPkgs = inputs.nixpkgs.legacyPackages.${system};
@@ -34,15 +35,38 @@ let
 
     # Optional filetypes
     ${lib.optionalString haveFiletypes ''
-    while IFS= read -r f; do
-      install -Dm444 "$f" "$outdir/filetypes/$(basename "$f")"
-    done < <(find ${./filetypes} -type f -name '*.kak' -print)
+      while IFS= read -r f; do
+        install -Dm444 "$f" "$outdir/filetypes/$(basename "$f")"
+      done < <(find ${./filetypes} -type f -name '*.kak' -print)
     ''}
   '';
 
   kakouneLauncher = fullPkgs.writeShellScriptBin "kak" ''
     exec -a "$0" "${fullPkgs.kakoune}/bin/kak" "$@"
   '';
+
+  kakInNewTerm = pkgs.writeShellScriptBin "kakInNewTerm" ''
+    $TERM sh -c "kak $@" &>/dev/null &
+  '';
+
+  kakouneDesktop = pkgs.writeTextDir "share/applications/kakoune.desktop" (
+    lib.generators.toINI { } {
+      "Desktop Entry" = {
+        Name = "Kakoune";
+        Type = "Application";
+        TryExec = "kak";
+        Exec = "${kakInNewTerm}/bin/kakInNewTerm %U";
+        Terminal = true;
+        Icon = "kakoune";
+        Comment = "Edit text";
+        GenericName = "Text Editor";
+        StartupNotify = true;
+        MimeType = "text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;text/markdown;text/x-python;application/x-yaml;application/json;text/rust;text/vnd.trolltech.linguist;application/javascript";
+        Catagories = "Development;TextEditor;";
+        StartupWMClass = "Kakoune";
+      };
+    }
+  );
 
   wm = inputs.wrapper-manager.lib {
     pkgs = fullPkgs;
@@ -52,14 +76,15 @@ let
         wrappers.kakoune = {
           wrapperType = "shell";
           basePackage = kakouneLauncher;
-          extraPackages =
-            [
-              fullPkgs.kakoune
-              config
-              clipboard
-            ]
-            ++ lspPkgs
-            ++ fmtPkgs;
+          extraPackages = [
+            fullPkgs.kakoune
+            config
+            kakInNewTerm
+            kakouneDesktop
+            clipboard
+          ]
+          ++ lspPkgs
+          ++ fmtPkgs;
           env = {
             KAKOUNE_CONFIG_DIR.value = "${config}/share/kak";
           };
